59788c40debee71291def6d8669048df6a453b9f
Manas Gupta <manas94gupta@gmail.com>
2017-07-18 11:21:22 +0530

    add sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-// import Sidebar from './Sidebar.js'
+import Sidebar from './components/Sidebar.js'
 
 class App extends Component {
   render() {
     return (
-        <div></div>
+        <Sidebar />
     );
   }
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import './Sidebar.css'
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <aside>
+                <div className="nav-wrapper">
+                    <h1>TrakM</h1>
+
+                    <ul className="nav-ul-top">
+                        <li><i className="fa fa-tachometer" aria-hidden="true"></i> Overview</li>
+                        <li><i className="fa fa-area-chart" aria-hidden="true"></i> Frontend</li>
+                        <li><i className="fa fa-pie-chart" aria-hidden="true"></i> Backend</li>
+                    </ul>
+
+                    <ul className="nav-ul-bottom">
+                        <li><i className="fa fa-home" aria-hidden="true"></i> Home</li>
+                        <li><i className="fa fa-cog" aria-hidden="true"></i> Settings</li>
+                        <li><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</li>
+                    </ul>
+
+                </div>
+            </aside>
+        )
+    }
+}
+
+export default Sidebar
