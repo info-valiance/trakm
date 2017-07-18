543613722d90eb990972337baeda8e86b7c84e44
Manas Gupta <manas94gupta@gmail.com>
2017-07-18 18:14:35 +0530

    Header component styling and layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,11 @@ class Header extends Component {
             <nav className="header-navbar">
                 <div className="header-wrapper">
                     <h1>TrakM</h1>
+                    <div className="header-filler"></div>
+                    <div className="header-elements">
+                        <i className="fa fa-bell-o" aria-hidden="true"></i>
+                        <span className="header-user-name">John Doe ▾</span>
+                    </div>
                 </div>
             </nav>
         )
