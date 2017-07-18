1fb561e56dd7b1505ff97b7da538b45f12907ebc
Manas Gupta <manas94gupta@gmail.com>
2017-07-18 12:03:55 +0530

    add react-router and add link component to sidebar links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Sidebar.css'
 
 class Sidebar extends Component {
@@ -9,15 +10,51 @@ class Sidebar extends Component {
                     <h1><i className="fa fa-bars" aria-hidden="true"></i> TrakM</h1>
 
                     <ul className="nav-ul-top">
-                        <li><i className="fa fa-tachometer" aria-hidden="true"></i> Overview</li>
-                        <li><i className="fa fa-area-chart" aria-hidden="true"></i> Frontend</li>
-                        <li><i className="fa fa-pie-chart" aria-hidden="true"></i> Backend</li>
+                        <li>
+                            <Link
+                                to="/overview"
+                            >
+                                <i className="fa fa-tachometer" aria-hidden="true"></i> Overview
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/frontend"
+                            >
+                                <i className="fa fa-area-chart" aria-hidden="true"></i> Frontend
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/backend"
+                            >
+                                <i className="fa fa-pie-chart" aria-hidden="true"></i> Backend
+                            </Link>
+                        </li>
                     </ul>
 
                     <ul className="nav-ul-bottom">
-                        <li><i className="fa fa-home" aria-hidden="true"></i> Home</li>
-                        <li><i className="fa fa-cog" aria-hidden="true"></i> Settings</li>
-                        <li><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</li>
+                        <li>
+                            <Link
+                                to="/"
+                            >
+                                <i className="fa fa-home" aria-hidden="true"></i> Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/settings"
+                            >
+                                <i className="fa fa-cog" aria-hidden="true"></i> Settings
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/logout"
+                            >
+                                <i className="fa fa-sign-out" aria-hidden="true"></i> Logout
+                            </Link>
+                        </li>
                     </ul>
 
                 </div>
