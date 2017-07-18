75b75185608729c9be13f2d49d6c5dc45bab6272
Manas Gupta <manas94gupta@gmail.com>
2017-07-18 11:16:20 +0530

    include fonts and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+// import Sidebar from './Sidebar.js'
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+        <div></div>
     );
   }
 }
