440f62fc053d2b7272ff9e63532b86e36265e37f
Manas Gupta <manas94gupta@gmail.com>
2017-07-18 18:42:45 +0530

    Sidebar changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -34,22 +34,22 @@ class Sidebar extends Component {
                         &nbsp;
                         <span
                             className="sidebar-toggle-items"
-                            style={{
-                                display: `${this.state.expandSidebar ? 'inline-block' : 'none'}`
-                            }}
+                            // style={{
+                            //     display: `${this.state.expandSidebar ? 'inline-block' : 'none'}`
+                            // }}
                         >TrakM</span>
                     </h1>
 
                     <ul className="nav-ul-top">
-                        <NavbarLiLink to="/overview" icon="fa fa-tachometer" label="Overview" expandSidebar="this.state.expandSidebar" />
-                        <NavbarLiLink to="/frontend" icon="fa fa-area-chart" label="Frontend" expandSidebar="this.state.expandSidebar" />
-                        <NavbarLiLink to="/backend" icon="fa fa-pie-chart" label="Backend" expandSidebar="this.state.expandSidebar" />
+                        <NavbarLiLink to="/overview" icon="fa fa-tachometer" label="Overview" expandSidebar={this.state.expandSidebar} />
+                        <NavbarLiLink to="/frontend" icon="fa fa-area-chart" label="Frontend" expandSidebar={this.state.expandSidebar} />
+                        <NavbarLiLink to="/backend" icon="fa fa-pie-chart" label="Backend" expandSidebar={this.state.expandSidebar} />
                     </ul>
 
                     <ul className="nav-ul-bottom">
-                        <NavbarLiLink activeOnlyWhenExact={true} to="/" icon="fa fa-home" label="Home" expandSidebar="this.state.expandSidebar" />
-                        <NavbarLiLink to="/settings" icon="fa fa-cog" label="Settings" expandSidebar="this.state.expandSidebar" />
-                        <NavbarLiLink to="/logout" icon="fa fa-sign-out" label="Logout" expandSidebar="this.state.expandSidebar" />
+                        <NavbarLiLink activeOnlyWhenExact={true} to="/" icon="fa fa-home" label="Home" expandSidebar={this.state.expandSidebar} />
+                        <NavbarLiLink to="/settings" icon="fa fa-cog" label="Settings" expandSidebar={this.state.expandSidebar} />
+                        <NavbarLiLink to="/logout" icon="fa fa-sign-out" label="Logout" expandSidebar={this.state.expandSidebar} />
 
 
                         {/* previous way of rendering li with link */}
@@ -77,20 +77,20 @@ class Sidebar extends Component {
 
 // Custom link creating component which will match the path apply active class to matching element wrapper
 const NavbarLiLink = ({ label, icon, to, activeOnlyWhenExact, expandSidebar }) => (
-  <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => (
-    <li className={match ? 'navbar-li-active' : ''}>
-        <Link to={to}>
-            <i className={icon} aria-hidden="true"></i>
-            &nbsp;
-            <span
-                className="sidebar-toggle-items"
-                style={{
-                display: `${expandSidebar ? 'inline-block' : 'none'}`
-            }}
-            >{label}</span>
-        </Link>
-  </li>
-  )}/>
+    <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => (
+        <li className={match ? 'navbar-li-active' : ''}>
+            <Link to={to}>
+                <i className={icon} aria-hidden="true"></i>
+                &nbsp;
+                <span
+                    className="sidebar-toggle-items"
+                    // style={{
+                    //     display: `${expandSidebar ? 'inline-block' : 'none'}`
+                    // }}
+                    >{label}</span>
+                </Link>
+        </li>
+    )}/>
 )
 
 export default Sidebar
