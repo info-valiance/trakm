b29f10b24b0f011fab9fd2c0c10fa2d34c604fc3
Manas Gupta <manas94gupta@gmail.com>
2017-07-19 16:13:31 +0530

    leave victory pie chart and move to try recharts

--- src/components/VicPieChart.js
+++ src/components/VicPieChart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { VictoryPie } from 'victory';
+import { VictoryPie, VictoryLegend } from 'victory';
 
 class VicPieChart extends Component {
     constructor(props) {
@@ -21,28 +21,39 @@ class VicPieChart extends Component {
 
     render() {
         return (
-            <VictoryPie
-                data={this.state.data}
-                colorScale={['#f9705d', 'gold', '#63f763']}
-                // radius of donut hole
-                innerRadius={100}
-                // space between multiple arcs
-                padAngle={3}
-                // animate buggy for VictoryPie till version 0.21.2
-                // animate={{ duration: 500, onLoad: {duration: 2000} }}
-                // To position the labels from the center
-                labelRadius={112}
-                // labels text
-                labels={(data) => `${data.y}`}
-                style={{
-                    labels: {
-                        fill: "#fff" ,
-                        fontSize: "18px",
-                        fontFamily: "Open Sans",
-                        fontWeight: "bold"
-                    }
-                }}
-            />
+            <div>
+                <VictoryPie
+                    data={this.state.data}
+                    colorScale={['#f9705d', 'gold', '#63f763']}
+                    // radius of donut hole
+                    innerRadius={100}
+                    // space between multiple arcs
+                    padAngle={3}
+                    // animate buggy for VictoryPie till version 0.21.2
+                    // animate={{ duration: 500, onLoad: {duration: 2000} }}
+                    // To position the labels from the center
+                    labelRadius={112}
+                    // labels text
+                    labels={(data) => `${data.y}`}
+                    style={{
+                        labels: {
+                            fill: "#fff" ,
+                            fontSize: "18px",
+                            fontFamily: "Open Sans",
+                            fontWeight: "bold"
+                        }
+                    }}
+                />
+                <VictoryLegend
+                    data={[
+                        { name: "Low Performing", symbol: { type: 'square'} },
+                        { name: "Fairly Performing", symbol: { type: 'square'} },
+                        { name: "High Performing", symbol: { type: 'square'} }
+                    ]}
+                    colorScale={['#f9705d', 'gold', '#63f763']}
+                    orientation="horizontal"
+                />
+            </div>
         )
     }
 }
