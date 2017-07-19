ae3651810b9f8aa096d931758efbb992c967a3b8
Manas Gupta <manas94gupta@gmail.com>
2017-07-19 18:19:05 +0530

    home page pie charts

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,22 +4,67 @@ import RechartPie from './RechartPie.js'
 import '../css/Home.css'
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data1: [
+                { name: "Low Performing", value: 40 },
+                { name: "Fairly Performing", value: 60 },
+                { name: "High Performing", value: 100 }
+            ],
+            data2: [
+                { name: "0-3 months", value: 30 },
+                { name: "3-6 months", value: 25 },
+                { name: "6-9 months", value: 15 },
+                { name: "9-12 months", value: 10 },
+                { name: "12-18 months", value: 10 },
+                { name: "18-24 months", value: 7 },
+                { name: "24+ months", value: 3 }
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="home-content-wrapper">
                 <div className="flex-box home-addmodel-graph-wrapper">
                     <div className="flex-items home-graphs">
                         <div className="card-accent"></div>
+                        <div className="home-graph-part"></div>
                         <div className="flex-box">
                             <div className="flex-items perf-dist-wrapper">
                                 <h1>Performance Distribution of Models</h1>
                                 {/* <VicPieChart /> */}
-                                <RechartPie />
+                                <RechartPie
+                                    data={this.state.data1}
+                                    dataKey="value"
+                                    startAngle={90}
+                                    endAngle={-270}
+                                    cx="50%"
+                                    cy="50%"
+                                    innerRadius={65}
+                                    outerRadius={100}
+                                    fill="#82ca9d"
+                                    label="label"
+                                    colors ={['#f9705d', 'gold', '#63f763']}
+                                />
                             </div>
                             <div className="flex-items age-dist-wrapper">
                                 <h1>Age Distribution of Models</h1>
                                 {/* <VicPieChart /> */}
-                                <RechartPie />
+                                <RechartPie
+                                    data={this.state.data2}
+                                    dataKey="value"
+                                    startAngle={90}
+                                    endAngle={-270}
+                                    cx="50%"
+                                    cy="50%"
+                                    innerRadius={65}
+                                    outerRadius={100}
+                                    fill="#82ca9d"
+                                    label="label"
+                                    colors ={['#006699', '#0088cc', '#00aaff', '#33bbff', '#66ccff', '#80d4ff', '#b3e6ff']}
+                                />
                             </div>
                         </div>
                     </div>
--- src/components/RechartPie.js
+++ src/components/RechartPie.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts'
 
 class RechartPie extends Component {
     render() {
-        const colors = ['#f9705d', 'gold', '#63f763'];
-        const data = [
-            { name: "Low Performing", value: 40 },
-            { name: "Fairly Performing", value: 60 },
-            { name: "High Performing", value: 100 }
-        ]
+        let colors = this.props.colors
 
         return (
-            <ResponsiveContainer width="100%" height={350}>
-                <PieChart width={730} height={250}>
-                    <Pie dataKey="value" data={data} startAngle={90} endAngle={-270} cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label>
+            <ResponsiveContainer width="100%" height={300}>
+                <PieChart>
+                    <Pie
+                        data={this.props.data}
+                        dataKey={this.props.dataKey}
+                        startAngle={this.props.startAngle}
+                        endAngle={this.props.endAngle}
+                        cx={this.props.cx}
+                        cy={this.props.cy}
+                        innerRadius={this.props.innerRadius}
+                        outerRadius={this.props.outerRadius}
+                        fill={this.props.fill}
+                        label={this.props.label}
+                    >
                         {
-                            data.map((entry, index) => <Cell key={index} fill={colors[index % colors.length]}/>)
+                            this.props.data.map((entry, index) => <Cell key={index} fill={colors[index % colors.length]}/>)
                         }
                     </Pie>
+                    <Legend verticalAlign="bottom"
+                        iconType="circle"
+                        iconSize={12}
+                        wrapperStyle={{
+                            fontSize: "12px"
+                        }}
+                    />
                 </PieChart>
             </ResponsiveContainer>
         )
