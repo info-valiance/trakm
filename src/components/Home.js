63088190c726fcc3ff8472ac0270daedc7c4069c
Manas Gupta <manas94gupta@gmail.com>
2017-07-19 12:30:35 +0530

    add a Home component for all home route content and create a basic page layout

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import VicPieChart from './VicPieChart.js'
+import '../css/Home.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-content-wrapper">
+                <div className="flex-box">
+                    <div className="home-graphs">ds</div>
+                    <div className="home-add-model">kd</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import VicPieChart from './VicPieChart.js'
+import React, { Component } from 'react'
+import Home from './Home.js'
 import '../css/Main.css'
 
 class Main extends Component {
     render() {
         return (
             <main className="main-section">
-                <VicPieChart />
+                <Home />
             </main>
         )
     }
