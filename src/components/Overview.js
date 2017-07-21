b5f0a8192d9d49470d650b6b92ff033df9185d7d
Manas Gupta <manas94gupta@gmail.com>
2017-07-21 11:32:27 +0530

    page head with current window

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,7 +7,13 @@ class Overview extends Component {
         return (
             <div className="overview-content-wrapper">
                 <div className="flex-box overview-card overview-head">
-
+                    <div className="flex-items page-name">
+                        Model Name: Overview
+                    </div>
+                    <div className="flex-items page-head-filler"></div>
+                    <div className="flex-items page-current-window">
+                        Current Window
+                    </div>
                 </div>
                 <div className="flex-box overview-card">
                     <div className="flex-items overview-gains-chart">
